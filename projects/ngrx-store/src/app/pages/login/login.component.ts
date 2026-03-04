0600426cebf53fa00e4ac394eceff39edf04aa3a
Edwin Nemeguen <95711147+PhoenixDwarf@users.noreply.github.com>
2026-03-04 12:56:09 -0500

    feat: implement signal form handling in login component with error display

--- projects/ngrx-store/src/app/pages/login/login.component.ts
+++ projects/ngrx-store/src/app/pages/login/login.component.ts
@@ -1,42 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { Button } from '../../shared/components/button';
 import { RouterLink } from '@angular/router';
+import { form, FormField, minLength, required } from '@angular/forms/signals';
+import { JsonPipe } from '@angular/common';
+import { FormErrors } from '../../shared/components/form-errors';
 
 @Component({
   selector: 'app-login',
-  imports: [Button, RouterLink],
+  imports: [Button, RouterLink, FormField, FormErrors],
   host: { class: 'min-h-screen flex items-center justify-center bg-slate-100 p-4' },
   template: `
     <form
       class="w-full max-w-sm mx-auto bg-white rounded-lg shadow-md p-6 flex flex-col gap-6"
-      autocomplete="off"
+      (submit)="onSubmit($event)"
     >
       <h2 class="text-2xl font-bold text-center mb-2">Sign in</h2>
       <div class="flex flex-col gap-2">
         <label for="username" class="text-sm font-medium text-gray-700">Username</label>
         <input
           id="username"
-          name="username"
           type="text"
+          [formField]="loginForm.username"
           class="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-slate-500"
-          required
           autocomplete="username"
           placeholder="Enter your username"
         />
+        <app-form-errors [control]="loginForm.username()" />
       </div>
       <div class="flex flex-col gap-2">
         <label for="password" class="text-sm font-medium text-gray-700">Password</label>
         <input
           id="password"
-          name="password"
           type="password"
+          [formField]="loginForm.password"
           class="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-slate-500"
-          required
           autocomplete="current-password"
           placeholder="Enter your password"
         />
+        <app-form-errors [control]="loginForm.password()" />
       </div>
-      <button type="submit" class="w-full" appButton variant="primary">Login</button>
+      <button
+        type="submit"
+        class="w-full"
+        appButton
+        variant="primary"
+        [disabled]="loginForm().invalid() && loginForm().touched()"
+      >
+        Login
+      </button>
       <p class="text-sm text-center text-slate-500 mt-4">
         Don't have an account?
         <a routerLink="/register" class="text-slate-500 font-medium underline">Register</a>
@@ -44,4 +55,24 @@ import { RouterLink } from '@angular/router';
     </form>
   `,
 })
-export default class LoginComponent {}
+export default class LoginComponent {
+  loginModel = signal({
+    username: '',
+    password: '',
+  });
+
+  loginForm = form(this.loginModel, (rootPath) => {
+    required(rootPath.username, { message: 'Username is required' });
+    required(rootPath.password, { message: 'Password is required' });
+    minLength(rootPath.password, 6, { message: 'Password must be at least 6 characters' });
+  });
+
+  onSubmit(event: Event) {
+    event.preventDefault();
+    if (this.loginForm().valid()) {
+      console.log('Login Data: ', this.loginForm().value());
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+}
